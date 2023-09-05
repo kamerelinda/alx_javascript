be9efd489f0a7e9bdf4b8c371fcac5a981163235
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-09-05 13:29:55 +0300

    using let to replace value

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+// Update this script above to replace the value 12 with 89
+myObject = {
+  type: 'object',
+  value: 89
+};
+console.log(myObject);
