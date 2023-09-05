e146c1a0fdb33a2f335e397efda57b9aa421d987
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-09-05 13:03:53 +0300

    print 3 strings

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+// script that prints 3 lines
+
+const first = ('C is fun');
+const second = ('Python is cool');
+const third = ('JavaScript is amazing');
+
+console.log(first);
+console.log(second);
+console.log(third);
