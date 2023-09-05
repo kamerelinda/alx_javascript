a1beb7a8d1073092ad0fec093443e6018304fb7e
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-09-05 13:23:02 +0300

    print array using loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+// print an array
+
+const myLang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lleng = myLang.length;
+
+for (let i = 0; i < lleng; i++) {
+  console.log(myLang[i]);
+}
