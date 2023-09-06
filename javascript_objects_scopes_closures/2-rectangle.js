0af63e5530e677f3ae4f2ab3ace3e3455f9fce13
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-09-06 13:39:26 +0300

    add condition to params

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+// class defines a rectangle
+class Rectangle {
+    constructor (w, h) {
+        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            return 'Rectangle{}';
+        }
+        this.width = w;
+        this.height = h;
+    }
+}
+module.exports = Rectangle;
