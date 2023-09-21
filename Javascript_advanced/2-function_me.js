787633e57e19a25a6a5c155ce15fbd649db5e0e1
lindakamere <125465749+kamerelinda@users.noreply.github.com>
2023-09-22 02:08:58 +0300

    closure

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+function welcomeMessage(fullName) {
+    alert(`Welcome ${fullName}`);
+}
+let guillaume = welcomeMessage('Guillaume');
+let alex = welcomeMessage('Alex');
+let fred = welcomeMessage('Fred');
